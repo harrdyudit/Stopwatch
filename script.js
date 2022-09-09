1147401da232ec0fd49f2825278c7bd10a5c7ffe
udit <sharmaudit262@gmail.com>
2022-09-09 11:55:17 +0530

    " makeRespnsive"

--- script.js
+++ script.js
@@ -1,66 +1,69 @@
-let hr=0;
-let min=0
-let sec=0;
-let count=0;
-let timer=false;
+// declare All the Variable
+let hr = 0;
+let min = 0;
+let sec = 0;
+let count = 0;
+let timer = false;
 
-function start(){
-    timer=true;
-    stopWatch();
-
+// function for start
+function start() {
+  timer = true;
+  stopWatch();
 }
-function stops(){
-    timer=false;
-
+// function for stop
+function stops() {
+  timer = false;
 }
-function reset(){
-    timer=false;
-    hr=0;
-    min=0;
-    sec=0;
-    count=0
-    document.getElementById("hr").innerHTML="00";
-    document.getElementById("min").innerHTML="00";
-    document.getElementById("sec").innerHTML="00";
-    document.getElementById("count").innerHTML="00";
-    
+// function for reset
+function reset() {
+  timer = false;
+  hr = 0;
+  min = 0;
+  sec = 0;
+  count = 0;
+
+  document.getElementById("hr").innerHTML = "00";
+  document.getElementById("min").innerHTML = "00";
+  document.getElementById("sec").innerHTML = "00";
+  document.getElementById("count").innerHTML = "00";
 }
 
-function stopWatch(){
-    if(timer==true){
-        count=count+1;
-        if(count==100){
-            sec+=1;
-            count=0;
-        }
-        if(sec==60){
-            min+=1;
-            sec=0;
-        }
-        if(min==60){
-            hr+=1;
-            min=0;;
-        }
-        let hrString=hr;
-        let minString=min;
-        let secString=sec;
-        let countString=count;
-        if(hr<10){
-            hrString="0"+hrString;
-        }
-        if(min<10){
-            minString="0"+minString
-        }
-        if(sec<10){
-            secString="0"+secString
-        }
-        if(count<10){
-            countString="0"+countString
-        }
-        document.getElementById("hr").innerHTML=hrString
-        document.getElementById("min").innerHTML=minString;
-        document.getElementById("sec").innerHTML=secString
-        document.getElementById("count").innerHTML=countString;
-        setTimeout("stopWatch()", 10);
+function stopWatch() {
+  if (timer == true) {
+    count = count + 1;
+    if (count == 100) {
+      sec += 1;
+      count = 0;
     }
+    if (sec == 60) {
+      min += 1;
+      sec = 0;
+    }
+    if (min == 60) {
+      hr += 1;
+      min = 0;
+    }
+    let hrString = hr;
+    let minString = min;
+    let secString = sec;
+    let countString = count;
+    if (hrString < 10) {
+      hrString = "0" + hrString;
+    }
+    if (minString < 10) {
+      minString = "0" + minString;
+    }
+    if (secString < 10) {
+      secString = "0" + secString;
+    }
+    if (countString < 10) {
+      countString = "0" + countString;
+    }
+
+    document.getElementById("hr").innerHTML = hrString;
+    document.getElementById("min").innerHTML = minString;
+    document.getElementById("sec").innerHTML = secString;
+    document.getElementById("count").innerHTML = countString;
+    setTimeout("stopWatch()", 10);
+  }
 }
